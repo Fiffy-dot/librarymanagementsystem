76fa4a01c2da667dd3852d604326241932a42026
Fiona Mukuhi <mukuhifiona@gmail.com>
2020-12-03 13:45:41 +0200

    The draft

--- books.py
+++ books.py
@@ -1,4 +1,11 @@
 class Book:
+    """
+    -----
+    Book
+
+    This is the class that instantiates the books
+
+    """
     def __init__(self, name, id, status, author):
         self.name = name
         self.id = id
